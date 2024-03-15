4ce1847e9a4f0e234aff439d0521a6b68e76ec2a
DDanii <ddani1995@gmail.com>
2024-03-15 11:22:20 +0000

    data gatherer update

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const http = require('http');
 const jsdom = require("jsdom");
 const fs = require('fs');
 const file_database = 'history.json';
-const nosell = 'no sell';
-const nobuy = 'no buy';
 let current_date = new Date();
 
 jsdom.JSDOM.fromURL("http://188.156.96.32/TraderPrice/TraderPrice.php").then(dom => {
@@ -20,19 +18,45 @@ jsdom.JSDOM.fromURL("http://188.156.96.32/TraderPrice/TraderPrice.php").then(dom
         
         if (cells.length > 4) {
             
-            var name = cells[0].innerHTML.toString();
+            var name = cells[0].innerHTML.toString().trim();
             var bprice = parseFloat(cells[2].textContent);
             var sprice = parseFloat(cells[4].textContent);
             var entry = {
-                'buy': bprice,
-                'sell': sprice,
+                'buy': Number.isFinite(bprice) ? bprice : null,
+                'sell': Number.isFinite(sprice) ? sprice : null,
                 'timestamp': current_date.getTime()
             };
 
             if (database[name]) {
-                database[name]['prices'].push(entry);
+                var prices = database[name]['prices'];
+                if (prices.length > 0) {
+
+                    var existed_entry_index = prices.findIndex( (x) => x.timestamp == entry.timestamp );
+                    if (existed_entry_index > -1) {
+                        var e = prices[existed_entry_index];
+                        if (e.sell != entry.sell) e.sell = Math.max(e.sell, entry.sell);
+                        if (e.buy != entry.buy) e.buy = Math.min(e.buy, entry.buy);
+                    } else {
+                        database[name]['prices'].push(entry);
+                    }                    
+                } else {
+                    database[name]['prices'].push(entry);
+                }
             } else {
                 database[name] = { 'prices': [ entry ] };
+            }
+        }
+    }
+
+    // remove latest price if didnt changed
+    for (var k in database) {
+        var prices = database[k]['prices'];
+        if (prices.length > 1) {
+            var last = prices[prices.length - 1];
+            var prev = prices[prices.length - 2];
+
+            if (last.buy == prev.buy && last.sell == prev.sell) {
+                prices.splice(prices.length - 1, 1);
             }
         }
     }
